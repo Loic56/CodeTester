59c3f2f301667f1e99837143770339bbe1ee3043
Loic56 <loic.crusson@live.fr>
2014-01-28 15:25:47 -1200

    last updated

--- src/main/java/web/forms/CreateTest.java
+++ src/main/java/web/forms/CreateTest.java
@@ -7,6 +7,7 @@ package web.forms;
 
 import dao.ICategorieDao;
 import dao.IPassageDao;
+import dao.IPropositionDao;
 import dao.IRubriqueDao;
 import dao.ITestDao;
 import java.io.Serializable;
@@ -23,13 +24,16 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import jpa.Categorie;
+import jpa.Proposition;
 import jpa.Question;
 import jpa.Rubrique;
 import jpa.Test;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.event.NodeUnselectEvent;
+import org.primefaces.event.SelectEvent;
 import org.primefaces.model.DefaultTreeNode;
 import org.primefaces.model.TreeNode;
+import org.primefaces.model.UploadedFile;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import tools.Document;
@@ -44,7 +48,7 @@ import tools.Utils;
 public class CreateTest implements Serializable {
 
     // page1
-    private String type;
+    private String type, theType;
     private String categorie;
     private String theme;
     private String duree;
@@ -54,36 +58,54 @@ public class CreateTest implements Serializable {
     //page2
     private String nomRubrique;
     private List<Rubrique> listRubrique;
+    private Rubrique rubToSup;
 
-    // le test en session
+    private String theProposition;
+    private boolean propositionIsChecked;
+    private List<Proposition> listProposition;
+
+    // le test en cours
     private Test theTest;
+
     // 
     private TreeNode root;
     private Document selectedDocument;
-
     private TreeNode[] selectedTreeNode;
 
+    // dao
     private ICategorieDao categorieDao = null;
     private ITestDao testDao = null;
     private IRubriqueDao rubriqueDao = null;
+    private IPropositionDao propositionDao = null;
 
     // création d'une question
     private String enonce;
     private String code_enonce;
 
     public CreateTest() {
-
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
         categorieDao = (ICategorieDao) ctx.getBean("categorieDao");
         testDao = (ITestDao) ctx.getBean("testDao");
         rubriqueDao = (IRubriqueDao) ctx.getBean("rubriqueDao");
+        propositionDao = (IPropositionDao) ctx.getBean("propositionDao");
 
         listRubrique = new ArrayList<Rubrique>();
         root = new DefaultTreeNode("root", null);
-
     }
 
-    public String creerRubrique() {
+    public void deleteRubrique() {
+        System.out.println("rubToSup :" + getRubToSup().getRubriquenom());
+        getListProposition().remove(getRubToSup());
+        String url = "http://localhost:8080/CodeTester/faces/createTest_2.xhtml";
+        Utils.redirect(url);
+    }
+
+    public void onRowSelect(SelectEvent event) {
+        Rubrique r = (Rubrique) event.getObject();
+        System.out.println("Suppression => id_rub = " + r.getRubriqueid());
+    }
+
+    public void creerRubrique() {
         System.out.println("creerRubrique");
         // on supprime tous les noeuds existants
         List<TreeNode> nodes = getRoot().getChildren();
@@ -93,7 +115,6 @@ public class CreateTest implements Serializable {
             // Use isLeaf() method to check doesn't have childs.
             i.remove();
         }
-
         Rubrique rubrique = new Rubrique();
         rubrique.setRubriquenom(getNomRubrique());
         rubrique.setTestid(getTheTest());
@@ -102,12 +123,10 @@ public class CreateTest implements Serializable {
         rubrique.setQuestionCollection(collection);
 
         Rubrique theRubrique = getRubriqueDao().create(rubrique);
-
         System.out.println("test : " + getTheTest());
 
         // toutes les rubriques pour un test
         List<Rubrique> list2 = getRubriqueDao().find(getTheTest());
-
         if (list2 != null) {
             System.out.println("Nombre de rubriques pour le test n°" + getTheTest().getTestid() + " : " + list2.size());
 
@@ -127,12 +146,12 @@ public class CreateTest implements Serializable {
             }
         }
 
-        String url = "http://localhost:8080/CodeTester/faces/createTest_2.xhtml";
-        Utils.redirect(url);
         //raz inputtext
         setNomRubrique("");
         getListRubrique().add(theRubrique);
-        return "createTest_2?faces-redirect=true";
+
+        String url = "http://localhost:8080/CodeTester/faces/createTest_2.xhtml";
+        Utils.redirect(url);
     }
 
     public String creerTest() {
@@ -149,10 +168,10 @@ public class CreateTest implements Serializable {
         } else if (cat.getCategorieid() == 3) {
             matiere = "Développement SQL";
         }
-        if (getType().equals("1")) {
-            type = "QCM";
+        if (getType().equals("2")) {
+            setTheType("QCM");
         } else if (getType().equals("1")) {
-            type = "CODE";
+            setTheType("CODE");
         }
         test.setNiveau(Integer.parseInt(getNiveau()));
         test.setTestDescription(getDescription());
@@ -167,9 +186,42 @@ public class CreateTest implements Serializable {
         return "createTest_2?faces-redirect=true";
     }
 
+    public void ajouterProposition() {
+        // type QCM
+        Proposition p = new Proposition();
+        p.setPropositionlibelle(getTheProposition());
+        String check = (isPropositionIsChecked() == true) ? "1" : "0";
+        p.setPropositionvrai(Short.valueOf(check));
+        p.setQuestionid(null);
+
+        System.out.println("libellé:" + getTheProposition() + " - vrai=" + check);
+
+        Proposition theProp = getPropositionDao().create(p);
+        getListProposition().add(theProp);
+        String url = "http://localhost:8080/CodeTester/faces/createTest_2.xhtml";
+        Utils.redirect(url);
+    }
+
     public void handleFileUpload(FileUploadEvent event) {
-        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
+        UploadedFile uploadedFile = event.getFile();
+        if (uploadedFile != null) {
+            System.out.println(uploadedFile.getFileName());
+        } else {
+            System.out.println("The file object is null.");
+        }
+//        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+//        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void handleFileUploadDeuz(FileUploadEvent event) {
+        UploadedFile uploadedFile = event.getFile();
+        if (uploadedFile != null) {
+            System.out.println(uploadedFile.getFileName());
+        } else {
+            System.out.println("The file object is null.");
+        }
+//        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+//        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 
     public void onNodeUnselect(NodeUnselectEvent event) {
@@ -413,4 +465,89 @@ public class CreateTest implements Serializable {
     public void setCode_enonce(String code_enonce) {
         this.code_enonce = code_enonce;
     }
+
+    /**
+     * @return the theType
+     */
+    public String getTheType() {
+        return theType;
+    }
+
+    /**
+     * @param theType the theType to set
+     */
+    public void setTheType(String theType) {
+        this.theType = theType;
+    }
+
+    /**
+     * @return the listProposition
+     */
+    public List<Proposition> getListProposition() {
+        return listProposition;
+    }
+
+    /**
+     * @param listProposition the listProposition to set
+     */
+    public void setListProposition(List<Proposition> listProposition) {
+        this.listProposition = listProposition;
+    }
+
+    /**
+     * @return the theProposition
+     */
+    public String getTheProposition() {
+        return theProposition;
+    }
+
+    /**
+     * @param theProposition the theProposition to set
+     */
+    public void setTheProposition(String theProposition) {
+        this.theProposition = theProposition;
+    }
+
+    /**
+     * @return the propositionIsChecked
+     */
+    public boolean isPropositionIsChecked() {
+        return propositionIsChecked;
+    }
+
+    /**
+     * @param propositionIsChecked the propositionIsChecked to set
+     */
+    public void setPropositionIsChecked(boolean propositionIsChecked) {
+        this.propositionIsChecked = propositionIsChecked;
+    }
+
+    /**
+     * @return the rubToSup
+     */
+    public Rubrique getRubToSup() {
+        return rubToSup;
+    }
+
+    /**
+     * @param rubToSup the rubToSup to set
+     */
+    public void setRubToSup(Rubrique rubToSup) {
+        this.rubToSup = rubToSup;
+    }
+
+    /*
+     * @return the propositionDao
+     */
+    public IPropositionDao getPropositionDao() {
+        return propositionDao;
+    }
+
+    /**
+     * @param propositionDao the propositionDao to set
+     */
+    public void setPropositionDao(IPropositionDao propositionDao) {
+        this.propositionDao = propositionDao;
+    }
+
 }
